1de161dd701178a0139d7302496af02a54d50dfe
sushidesu <hugemerge@gmail.com>
2021-10-08 15:25:43 +0900

    update link script

---
+++ scripts/colors.sh
@@ -0,0 +1,35 @@
+# ref: https://gist.github.com/jonsuh/3c89c004888dfc7352be
+
+# no color
+NC="\033[0m"
+# red
+C_RD="\033[0;31m"
+# green
+C_GR="\033[0;32m"
+# orange
+C_OR="\033[0;33m"
+# blue
+C_BL="\033[0;34m"
+# purple
+C_PR="\033[0;35m"
+# cyan
+C_CY="\033[0;36m"
+# light gray
+C_LGY="\033[0;37m"
+# dark gray
+C_DGY="\033[1;30m"
+# light red
+C_LRD="\033[1;31m"
+# light green
+C_LGR="\033[1;32m"
+# yellow
+C_YE="\033[1;33m"
+# light blue
+C_LBL="\033[1;34m"
+# light purple
+C_LPR="\033[1;35m"
+# light cyan
+C_LCY="\033[1;36m"
+# white
+C_WH="\033[1;37m"
+
--- scripts/link.sh
+++ scripts/link.sh
@@ -1,24 +1,78 @@
 #!/bin/bash
 
-EXCLUDE_PATH=(".git")
+EXCLUDE_PATH=(".git" ".config" "scripts")
 DOTPATH=$HOME/.dotfiles
 
+# enable echo in color
+source $DOTPATH/scripts/colors.sh
+
+function link_anywhere() {
+  local base_dir=$1
+  local link_dir=$2
+
+  function link_to_link_dir() {
+    local target_filename=$1
+    local base_filename=`basename $target_filename`
+    linking "$base_dir/$base_filename" "$link_dir/$base_filename"
+  }
+  list_dir $base_dir link_to_link_dir
+}
+
+function message_skip() {
+  echo -e "${C_BL}skip${NC} $1"
+}
+function message_link() {
+  echo -e "${C_GR}link${NC} $1 -> $2"
+}
+function linking() {
+  local from_name=$1
+  local to_name=$2
+
+  # validation for idempotence
+  if [ -h $to_name ]; then
+    message_skip "${to_name} is already linked"
+  elif [ -d $to_name ]; then
+    message_skip "${to_name} is directory"
+  else
+    # link
+    message_link $from_name $to_name
+    ln -s $from_name $to_name
+  fi
+}
+
+function list_dir() {
+  local target_dir=$1
+  local function_receives_filename=$2
+
+  for i in $target_dir/* $target_dir/.??*; do
+    if [ ! -d $i ] && [ ! -e $i ]; then
+      continue
+    elif is_exclude $i; then
+      continue
+    else
+      $function_receives_filename $i
+    fi
+  done
+}
+
 function is_exclude() {
+  local filename=$1
   for ex in "${EXCLUDE_PATH[@]}"; do
-    if [[ $DOTPATH/$ex == $1 ]]; then
+    if [[ $DOTPATH/$ex == $filename ]]; then
       return 0
     fi
   done
   return 1
 }
 
-for i in $DOTPATH/.??*; do
-  if is_exclude $i; then
-    continue
-  else
-    FILENAME=`basename $i`
-    echo "$i -> ~/$FILENAME"
-    ln -sf $i $HOME/$FILENAME
-  fi
-done
+function message() {
+  local target=$1
+  echo -e "${C_LGY}------ LINK \"${target}\"${NC}"
+}
+
+message "\$DOTPATH/*"
+link_anywhere $DOTPATH $HOME
+
+message "\$DOTPATH/config/*"
+link_anywhere $DOTPATH/.config $HOME/.config
 
