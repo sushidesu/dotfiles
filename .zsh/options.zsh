f57e81ef07a5e981e5603c4eceb289b4db60659d
sushidesu <hugemerge@gmail.com>
2023-05-20 23:13:00 +0900

    better history

--- .zsh/options.zsh
+++ .zsh/options.zsh
@@ -2,13 +2,19 @@
 unsetopt PROMPT_SP
 autoload -Uz compinit
 compinit
+
 setopt share_history
 setopt append_history
+setopt inc_append_history
+setopt extended_history
+export HISTTIMEFORMAT="[%F %T] "
+
 HISTFILE=$HOME/.zsh-history
-HISTSIZE=10000
-SAVEHIST=10000
+HISTSIZE=1000000000
+SAVEHIST=1000000000
 setopt hist_ignore_all_dups
 setopt hist_ignore_space
+
 setopt auto_cd
 function chpwd() { ls } # auto ls
 
